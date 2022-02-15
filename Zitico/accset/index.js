1641db782e1cc878b2267f485041f9689b154c20
Kim chi202 <xuanhiepqv1408@gmail.com>
2022-02-15 11:53:35 +0700

    bai tap

---
+++ Zitico/accset/index.js
@@ -0,0 +1,49 @@
+// Bài 1:
+// let a = 1;
+// if (a%1 == 0) {
+//     console.log('Yes');
+// } else {
+//     console.log('No');
+// }
+// bai 2
+// let a = 1;
+// switch (a) {
+//     case 1:
+//         console.log(1);
+        
+//     case 2:
+//             console.log(2);
+        
+//     case 3:
+//             console.log(3);
+        
+//     case 4:
+//             console.log(4);
+       
+//     case 5:
+//             console.log(5);
+        
+//     case 6:
+//             console.log(6);
+        
+//     case 7:
+//             console.log(7);
+        
+//     case 8:
+//             console.log(8);
+        
+//     case 9:
+//             console.log(9);
+        
+//     case 10:
+//             console.log(10);
+        
+
+//     default:
+//         break;
+// }
+// bai 5
+// let n = 10;
+
+
+
