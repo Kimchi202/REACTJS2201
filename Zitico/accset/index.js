063b668a81ff2d18e6b897da50cb1590512b87c6
Kim chi202 <xuanhiepqv1408@gmail.com>
2022-02-22 15:00:32 +0700

    bai tap

--- Zitico/accset/index.js
+++ Zitico/accset/index.js
@@ -1,49 +1,126 @@
-// Bài 1:
-// let a = 1;
-// if (a%1 == 0) {
-//     console.log('Yes');
-// } else {
-//     console.log('No');
-// }
-// bai 2
-// let a = 1;
-// switch (a) {
-//     case 1:
-//         console.log(1);
-        
-//     case 2:
-//             console.log(2);
-        
-//     case 3:
-//             console.log(3);
-        
-//     case 4:
-//             console.log(4);
-       
-//     case 5:
-//             console.log(5);
-        
-//     case 6:
-//             console.log(6);
-        
-//     case 7:
-//             console.log(7);
-        
-//     case 8:
-//             console.log(8);
-        
-//     case 9:
-//             console.log(9);
-        
-//     case 10:
-//             console.log(10);
-        
 
-//     default:
-//         break;
-// }
 // bai 5
 // let n = 10;
 
+// let n = 500;
+// let c = true;
+// if (n < 2 ) {
+//     c = false;
+// } else if (n == 2) {
+//    c = false;
+// } else if (n %2) {
+//     c= true;
+    
+// } else {
+//     for (let index = 3; index < Math.sqrt(n); index+=2) {
+//         if (n % index == 0) {
+//             c = false;
+//             break;
+//         }
+        
+//     }
+// }
+// if (c == true) {
+//     console.log('n la so nguyen to');
+// } else {
+//     console.log('n khong phai la so nguyen to');  
+// }
+// let sum = 0;
+// let n = 5;
+// for (let i = 1; i <= n; i+=2) {
+//     sum += i ;
 
-
+    
+// }
+// console.log(sum);
+// function isName() {
+//     console.log('Toi di code dao');
+// }
+// isName();
+// function SUM(a ,b) {
+//     console.log('Tong cua a va b la', a + b);
+// }
+// SUM (99 , 1)
+// function Sum(a , b , c) {
+//     return a + b + c;
+// }
+// let result = Sum(1, 2 ,3 )
+// console.log('Tong la', result);
+// bai1
+// function checkPrime(n) {
+// let c = true;
+// if (n < 2 ) {
+//     c = false;
+// } else if (n == 2) {
+//    c = false;
+// } else if (n %2) {
+//     c= true;
+    
+// } else {
+//     for (let index = 3; index < Math.sqrt(n); index+=2) {
+//         if (n % index == 0) {
+//             c = false;
+//             break;
+//         }
+        
+//     }
+// }
+// if (c == true) {
+//     console.log('n la so nguyen to');
+// } else {
+//     console.log('n khong phai la so nguyen to');
+// }
+// }
+// checkPrime(503);
+// bai7
+// function checkOdd(n) {
+//     let sum = 0;
+//     for (let index = 1; index < n; index+=2) {
+       
+        
+//     }
+// }
+// checkOdd(10);
+//  bai 3
+// function printEvent(n) {
+//     for (let index = 0; index < n; index+=2) {
+//         console.log(index);
+        
+//     }
+// }
+// printEvent(20);
+// bai 2
+// function printOdd(n) {
+//     for (let index = 1; index < n; index+=2) {
+//         console.log(index);
+        
+//     }
+// }
+// printOdd(20);
+// bai1
+// function printNumber(n) {
+//     for (let index = 0; index <= n; index++) {
+//         console.log(index);
+        
+//     }
+// }
+// printNumber(10);
+// function checkOddNumberForm(n) {
+//     let check = true;
+//     if (n % 2 == 0) {
+//         check = false;
+//     }else {
+//         for (let index = 1; index < n; index+=2) {
+//            if (n % index == 0) {
+//                check = true;
+//            }
+            
+//         }
+//     }
+// }
+// if (check = true) {
+//     console.log("Yes");
+// } else {
+//    console.log("No"); 
+// }
+// checkOddNumberForm(131);
